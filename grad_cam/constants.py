fd8cc1bdbccbedcc148f3736311be9e23bb7453d
Deshraj <deshrajdry@gmail.com>
2016-09-21 20:43:27 -0400

    constants modified

--- grad_cam/constants.py
+++ grad_cam/constants.py
@@ -1,13 +1,15 @@
 from django.conf import settings
 import os
 
-COCO_IMAGES_PATH = os.path.join(settings.MEDIA_URL, 'coco', 'val2014')
+COCO_IMAGES_PATH = os.path.join(settings.MEDIA_ROOT, 'coco', 'val2014')
+
+VQA_GPUID = -1
 
 VQA_CONFIG = {
     'proto_file': 'models/VGG_ILSVRC_19_layers_deploy.prototxt',
     'model_file': 'models/VGG_ILSVRC_19_layers.caffemodel',
     'input_sz': 224,
-    'backend': 'cudnn',
+    'backend': '',
     'layer_name': 'relu5_4',
     'model_path': 'VQA_LSTM_CNN/lstm.t7',
     'input_encoding_size': 200,
@@ -19,8 +21,15 @@ VQA_CONFIG = {
     'image_dir': os.path.join(settings.BASE_DIR, 'media', 'grad_cam', 'vqa')
 }
 
+
+if VQA_GPUID == -1:
+    VQA_CONFIG['backend'] = "nn"
+else:
+    VQA_CONFIG['backend'] = "cudnn"
+
 VQA_LUA_PATH = "visual_question_answering.lua"
 
+CLASSIFICATION_GPUID = 0
 
 CLASSIFICATION_CONFIG = {
     'proto_file': 'models/VGG_ILSVRC_16_layers_deploy.prototxt',
@@ -32,8 +41,15 @@ CLASSIFICATION_CONFIG = {
     'image_dir': os.path.join(settings.BASE_DIR, 'media', 'grad_cam', 'classification')
 }
 
+
 CLASSIFICATION_LUA_PATH = "classification.lua"
 
+if CLASSIFICATION_GPUID == -1:
+    CLASSIFICATION_CONFIG['backend'] = "nn"
+else:
+    CLASSIFICATION_CONFIG['backend'] = "cudnn"
+
+CAPTIONING_GPUID = -1
 
 CAPTIONING_CONFIG = {
     'input_sz': 224,
@@ -45,3 +61,8 @@ CAPTIONING_CONFIG = {
 }
 
 CAPTIONING_LUA_PATH = "captioning.lua"
+
+if CAPTIONING_GPUID == -1:
+    CAPTIONING_CONFIG['backend'] = "nn"
+else:
+    CAPTIONING_CONFIG['backend'] = "cudnn"
